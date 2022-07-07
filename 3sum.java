4c7ac4ccf8cf777c5458fa209d31983dd4324d73
SujanBoopathy <sujansaradha@gmail.com>
2022-07-07 17:03:01 +0530

    problem-solving

---
+++ 3sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+                  
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++){ 
+            int a=i,b=i+1,c=i+2;
+            while(c<nums.length){
+                int temp=nums[a]+nums[b]+nums[c];
+                if(temp==0){
+                    List<Integer> lst=new ArrayList<>();
+                    lst.add(nums[a]);
+                    lst.add(nums[b]);
+                    lst.add(nums[c]);
+                    res.add(lst);
+                }
+                b++;
+                c++;
+            }
+        }
+        return res;
+    }
+}
