de3b08b5a5435745156a8281bad1efe5fa701912
SujanBoopathy <sujansaradha@gmail.com>
2022-07-08 20:03:01 +0530

    problem-solving

--- count-and-say.java
+++ count-and-say.java
@@ -1,23 +1,27 @@
 class Solution {
     public String updateString(String s){
         Map<Character,Integer> mp=new HashMap<>();
+        List<Character> lst=new ArrayList<>();
         for(int i=0;i<s.length();i++){
             if(mp.containsKey(s.charAt(i))){
                 mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
             }
             else{
                 mp.put(s.charAt(i),1);
+                lst.add(s.charAt(i));
             }
         }
         String res="";
-        for(Map.Entry<Character,Integer> m:mp.entrySet()){
-            res+=m.getKey();
+        for(int i=0;i<lst.size();i++){
+            res+=Integer.toString(mp.get(lst.get(i)))+lst.get(i);
         }
         return res;
     }
     public String countAndSay(int n) {
         String s="1";
-        for(int i=0;i<n;i++){
+        if(n==1)
+            return s;
+        for(int i=1;i<n;i++){
             s=updateString(s);
         }
         return s;
