d321648dac79081f12c36f1145efdfcaae6712af
SujanBoopathy <sujansaradha@gmail.com>
2022-07-09 21:24:14 +0530

    problem-solving

--- first-and-last-position-of-element-in-sorted-arr.java
+++ first-and-last-position-of-element-in-sorted-arr.java
@@ -1,5 +1,23 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
+        int start=-1,end=-1,loc=0;
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                loc=mid;
+                break;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        int a[]=new int[2];
+        a[0]=loc;
+        a[1]=loc;
+        return a;
     }
 }
