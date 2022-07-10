127a4cef0c23dcf6ef4e16b793e0946585f49694
SujanBoopathy <sujansaradha@gmail.com>
2022-07-10 23:15:27 +0530

    problem-solving

--- decode-the-message.java
+++ decode-the-message.java
@@ -11,14 +11,7 @@ class Solution {
                 }
             }
         }
-        String res="";
-        for(int i=0;i<message.length();i++){
-            if(message.charAt(i)!=' '){
-                res+=a[mp.get(message.charAt(i))];
-            }
-            
-        }
-        return res;
+        
         
     }
 }
