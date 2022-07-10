51838fc671c34dbb1dbc5f64cdede6a924ea2bdf
SujanBoopathy <sujansaradha@gmail.com>
2022-07-10 23:14:05 +0530

    problem-solving

--- decode-the-message.java
+++ decode-the-message.java
@@ -1,20 +1,21 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        char alphabet[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char a[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         Map<Character,Integer> mp=new HashMap<>();
-        
-        char keys[]=new char[26];
         int index=0;
         for(int i=0;i<key.length();i++){
             if(key.charAt(i)!=' ' && index<26){
                 if(!mp.containsKey(key.charAt(i))){
-                    keys[index]=key.charAt(i);
+                    mp.put(key.charAt(i),index);
                     index++;
-                    mp.put(key.charAt(i),1);
                 }
             }
         }
+        String res="";
         for(int i=0;i<message.length();i++){
+            if(message.charAt(i)!=' '){
+                res+=a[mp.get(message.charAt(i))];
+            }
             
         }
         return res;
