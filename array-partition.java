cbcb37ecf3723462c7b770fa17a5a7ce25e27991
SujanBoopathy <sujansaradha@gmail.com>
2022-07-11 21:28:49 +0530

    problem-solving

--- array-partition.java
+++ array-partition.java
@@ -1,6 +1,16 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-        return 0;
+        int sum=0;
+        for(int i=nums.length-1;i>=(nums.length/2);i--){
+            if(nums[i]==nums[i-1]){
+                sum+=nums[i];
+                i--;
+            }
+            else{
+                sum+=nums[i];
+            }
+        }
+        return sum;
     }
 }
