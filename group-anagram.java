2233a7ebac115d3c1231ace206cc325aef153f87
SujanBoopathy <sujansaradha@gmail.com>
2022-07-11 19:52:38 +0530

    problem-solving

--- group-anagram.java
+++ group-anagram.java
@@ -1,5 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+        List<List<String>> res=new ArrayList<>();
+        int status[]=new int[strs.length];
+        for(int i=0;i<strs.length;i++)
+            status[i]=1;
+        for(int i=0;i<strs.length;i++){
+            List<String> temp=new ArrayList<>();
+            temp.add(strs[i]);
+            int count[]=new int[26];
+            for(int j=0;j<strs[i].length();j++){
+                count[strs[i].charAt(j)-'a']++;
+            }
+                        
+        }
+        return res;
     }
 }
