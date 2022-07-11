95b7adbcbab5bd5819591135d52b036f6814573b
SujanBoopathy <sujansaradha@gmail.com>
2022-07-11 20:20:32 +0530

    problem-solving

--- group-anagram.java
+++ group-anagram.java
@@ -4,8 +4,21 @@ class Solution {
         
         Map<String,ArrayList<String>> mp=new HashMap<>();
         for(String s:strs){
-            
+            char c[]=new char[26];
+            for(int i=0;i<s.length();i++){
+                c[s.charAt(i)-'a']++;
+            }
+            String temp=new String(c);
+            if(mp.containsKey(temp)){
+                mp.get(temp).add(s);
+            }
+            else{
+                ArrayList<String> ls=new ArrayList<>();
+                ls.add(s);
+                mp.put(temp,ls);
+            }
         }
+        result.addAll(mp.values());
         return result;
     }
 }
