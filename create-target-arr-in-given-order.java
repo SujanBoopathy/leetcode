25217ee327151a27084eda548469c7a49c798430
SujanBoopathy <sujansaradha@gmail.com>
2022-07-12 23:04:00 +0530

    problem-solving

--- create-target-arr-in-given-order.java
+++ create-target-arr-in-given-order.java
@@ -3,16 +3,7 @@ class Solution {
         int target[]=new int[nums.length];
         int visit[]=new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            if(visit[index[i]]!=1){
-                target[index[i]]=nums[i];
-                visit[index[i]]=1;
-            }
-            else{
-                for(int j=nums.length-1;j>index[i];j--){
-                    target[j]=target[j-1];
-                }
-                target[index[i]]=nums[i];
-            }
+            target[index[i]]=nums[i];
         }
         return target;
     }
