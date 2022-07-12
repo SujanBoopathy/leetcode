c39a91c1142f90720c910c117529dc10e1631b7f
SujanBoopathy <sujansaradha@gmail.com>
2022-07-12 22:41:40 +0530

    problem-solving

--- height-checker.java
+++ height-checker.java
@@ -6,7 +6,11 @@ class Solution {
         }
         Arrays.sort(arr);
         int count=0;
-        
+        for(int i=0;i<heights.length;i++){
+            if(arr[i]!=heights[i]){
+                count++;
+            }
+        }
         return count;
     }
 }
