a1d4b88ab30408ea1cec9763588f0bad9d89e414
SujanBoopathy <sujansaradha@gmail.com>
2022-07-12 22:41:25 +0530

    problem-solving

--- height-checker.java
+++ height-checker.java
@@ -1,5 +1,12 @@
 class Solution {
     public int heightChecker(int[] heights) {
+        int arr[]=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            arr[i]=heights[i];
+        }
+        Arrays.sort(arr);
+        int count=0;
         
+        return count;
     }
 }
