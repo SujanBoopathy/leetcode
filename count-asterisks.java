f6a46c19587507689198842f5538f9f0a81571c0
SujanBoopathy <sujansaradha@gmail.com>
2022-07-13 22:07:46 +0530

    problem-solving

--- count-asterisks.java
+++ count-asterisks.java
@@ -1,5 +1,15 @@
 class Solution {
     public int countAsterisks(String s) {
-        
+        int flag=0,count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='|'){
+                if(flag==1)
+                    flag=0;
+                else
+                    flag=1;
+            }
+            
+        }
+        return count;
     }
 }
