024c42c784cb3c957a02d825cde348cbc717bff3
SujanBoopathy <sujansaradha@gmail.com>
2022-07-14 19:57:52 +0530

    problem-solving

--- digitcount.java
+++ digitcount.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean digitCount(String num) {
-        int count[]=new int[num.length()];
+        int count[]=new int[10];
         
         for(int i=0;i<num.length();i++){
-            count[Integer.parseInt(s.charAt(i))]++;
+            count[Integer.parseInt(""+num.charAt(i))]++;
         }
         for(int i=0;i<num.length();i++){
-            if(count[i]==Integer.parseInt(s.charAt(i)))
+            if(count[i]==Integer.parseInt(""+num.charAt(i)))
                 continue;
             else
                 return false;
