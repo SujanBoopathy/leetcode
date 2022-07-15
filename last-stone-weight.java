27914945becf08bcb89a23c3fab6c3d27fb89506
SujanBoopathy <sujansaradha@gmail.com>
2022-07-15 20:50:17 +0530

    problem-solving

--- last-stone-weight.java
+++ last-stone-weight.java
@@ -3,34 +3,32 @@ class Solution {
         if(stones.length==1)
             return stones[0];
         while(true){
-            int max=stones[0],smax=0,mi=0,mj=0;
+            int max=0,smax=0,maxi=stones[0];
             for(int i=1;i<stones.length;i++){
                 if(max<stones[i]){
                     smax=max;
-                    mj=mi;
-                    max=stones[i];
-                    mi=i;
+                    max=i;
                 }
             }
-            if(smax==max){
-                stones[mi]=0;
-                stones[mj]=0;
+            if(stones[max]==stones[smax]){
+                stones[max]=0;
+                stones[smax]=0;
             }
             else{
-                stones[mi]=stones[mi]-stones[mj];
-                stones[mj]=0;
+                stones[max]=stones[max]-stones[smax];
+                stones[smax]=0;
             }
+            
             int flag=0;
             for(int i=0;i<stones.length;i++){
-                if(stones[i]!=0){
+                if(stones[i]>0)
                     flag++;
-                }
             }
             if(flag<=1)
                 break;
         }
         for(int i=0;i<stones.length;i++){
-            if(stones[i]!=0)
+            if(stones[i]>0)
                 return stones[i];
         }
         return 0;
