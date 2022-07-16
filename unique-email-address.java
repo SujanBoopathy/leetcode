49ee03e45f270f4d5ca3a3c0ae0caae35dfbb0d8
SujanBoopathy <sujansaradha@gmail.com>
2022-07-16 22:28:41 +0530

    problem-solving

--- unique-email-address.java
+++ unique-email-address.java
@@ -2,24 +2,9 @@ class Solution {
     public int numUniqueEmails(String[] emails) {
         Set<String> st=new HashSet<>();
         for(String s:emails){
-            String temp="";
-            int host=0,local=0;
-            for(int i=0;i<s.length();i++){
-                if(local==0 && s.charAt(i)!='.' && s.charAt(i)!='+'){
-                    temp+=s.charAt(i);
-                }
-                if(s.charAt(i)=='+' && local==0){
-                    local=1;
-                }
-                if(local==1 && s.charAt(i)=='@'){
-                    temp+=s.charAt(i);
-                    host=1;
-                }
-                if(host==1){
-                    temp+=s.charAt(i);
-                }
-            }
-            st.add(temp);
+            String a[]=s.split("@");
+            String local[]=a[0].split("\\+");
+            st.add(local[0].replace(".","")+"@"+a[1]);
         }
         return st.size();
     }
