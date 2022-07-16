26d52f05bff6c67f0c5ced89b151adcf2eca867c
SujanBoopathy <sujansaradha@gmail.com>
2022-07-16 22:07:02 +0530

    problem-solving

--- unique-email-address.java
+++ unique-email-address.java
@@ -5,7 +5,7 @@ class Solution {
             String temp="";
             int flag=0,flag1=1;
             for(int i=0;i<s.length();i++){
-                if(flag==0 && s.charAt(i)!='+'){
+                if(flag==0 && (s.charAt(i)=='+' || s.charAt(i)=='.')){
                     temp+=s.charAt(i);
                 }
                 else if(flag==0 && s.charAt(i)=='+'){
