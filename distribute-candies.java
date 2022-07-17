9049f33d1672ab4753ec665d38f9835978a1370a
SujanBoopathy <sujansaradha@gmail.com>
2022-07-17 21:04:07 +0530

    problem-solving

--- distribute-candies.java
+++ distribute-candies.java
@@ -1,5 +1,15 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-
+        int n=candyType.length;
+        Set<Integer> s=new HashSet<>();
+        for(int i:candyType)
+            s.add(i);
+        if((n/2)<=s.size()){
+            return n/2;
+        }        
+        else{
+            return s.size();
+        }
+        
     }
 }
