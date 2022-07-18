4a857fac8db30d23e2464b0cbbaa82c37edd5677
SujanBoopathy <sujansaradha@gmail.com>
2022-07-18 22:08:58 +0530

    problem-solving

--- zig-zag-conversion.java
+++ zig-zag-conversion.java
@@ -6,7 +6,9 @@ class Solution {
         for(int i=0;i<numRows;i++){
             for(int j=i;j<s.length();j+=step){
                 res+=s.charAt(j);
-                
+                if(i!=0 && i!=numRows-1 && j+step-2*i<s.length()){
+                    res+=s.charAt(j+step-2*i);
+                }
             }
         }
         return res;
