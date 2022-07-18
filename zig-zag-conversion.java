12831401313ceaeddb8a5b0da4dbc5742d5dd3c9
SujanBoopathy <sujansaradha@gmail.com>
2022-07-18 21:27:26 +0530

    problem-solving

---
+++ zig-zag-conversion.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        char ch[][]=new char[numRows][numRows];
+        String res="";
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<s.length();j++){
+                
+            }
+        }
+    }
+}
