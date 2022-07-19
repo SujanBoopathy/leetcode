b82015f4c522e8b09417237d6e245827fc05b68d
SujanBoopathy <sujansaradha@gmail.com>
2022-07-19 20:59:09 +0530

    problem-solving

--- search-in-rotated-sorted-arr.java
+++ search-in-rotated-sorted-arr.java
@@ -5,16 +5,7 @@ class Solution {
             if(nums[min]>nums[i])
                 min=i;
         }
-        int sorted[]=new int[nums.length];
-        int j=0;
-        for(int i=min;i<nums.length;i++){
-            sorted[j]=nums[i];
-            j++;
-        }
-        for(int i=0;i<min;i++){
-            sorted[j]=nums[i];
-            j++;
-        }
+        
         return min;
     }
 }
