afcfa79242081630258ea47d2fce1827a6d97172
SujanBoopathy <sujansaradha@gmail.com>
2022-07-19 20:59:58 +0530

    problem-solving

--- search-in-rotated-sorted-arr.java
+++ search-in-rotated-sorted-arr.java
@@ -6,8 +6,15 @@ class Solution {
             if(nums[mid]==target)
                 return mid;
             
+            if(nums[start]<=nums[mid]){
+                if(target>=nums[start] && target<nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
             
-        }
         return -1;
     }
 }
