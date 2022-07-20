e970b847bbfe56a60a57771bcaceaee6b262a461
SujanBoopathy <sujansaradha@gmail.com>
2022-07-20 21:06:52 +0530

    problem-solving

--- valid-sudoku.java
+++ valid-sudoku.java
@@ -12,9 +12,14 @@ class Solution {
         
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                int k=(i/3)*3+j/3;
+                if(board[i][j]!="."){
+                    int k=(i/3)*3+j/3;
                 if((rowSet[i].contains(board[i][j])) || colSet[i].contains(board[i][j]) || boxSet[i].contains(board[i][j])){
                     return false;
+                }
+                rowSet[i].add(board[i][j]);
+                colSet[i].add(board[i][j]);
+                boxSet[i].add(board[i][j]);
                 }
             }
         }
