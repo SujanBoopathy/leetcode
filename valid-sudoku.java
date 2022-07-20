0aa318f99a00cdb3718ade04cfdbf50730a547a8
SujanBoopathy <sujansaradha@gmail.com>
2022-07-20 21:03:58 +0530

    problem-solving

--- valid-sudoku.java
+++ valid-sudoku.java
@@ -9,6 +9,15 @@ class Solution {
             colSet[i]=new HashSet<Character>();
             boxSet[i]=new HashSet<Character>();
         }
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int k=(i/3)*3+j/3;
+                if((rowSet[i].contains(board[i][j])) || colSet[i].contains(board[i][j]) || boxSet[i].contains(board[i][j])){
+                    return false;
+                }
+            }
+        }
         return true;
         
     }
