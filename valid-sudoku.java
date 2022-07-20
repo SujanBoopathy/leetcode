d95bb5bfad49c0ce885db0faeeb1d88fde4e7942
SujanBoopathy <sujansaradha@gmail.com>
2022-07-20 21:07:56 +0530

    problem-solving

--- valid-sudoku.java
+++ valid-sudoku.java
@@ -12,7 +12,7 @@ class Solution {
         
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]!="."){
+                if(board[i][j] != '.'){
                     int k=(i/3)*3+j/3;
                 if((rowSet[i].contains(board[i][j])) || colSet[i].contains(board[i][j]) || boxSet[i].contains(board[i][j])){
                     return false;
