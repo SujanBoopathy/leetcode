0729b40cc8a057585cbd48daf1e6f2782fa7d562
SujanBoopathy <sujansaradha@gmail.com>
2022-08-07 20:17:35 +0530

    problem-solving

--- count-prefix-of-string.java
+++ count-prefix-of-string.java
@@ -2,15 +2,17 @@ class Solution {
     public int countPrefixes(String[] words, String s) {
         int count=0;
         for(String word : words ){
-            int flag=0;
-            for(int i=0;i<word.length() && i<s.length();i++){
-                if(s.charAt(i)!=word.charAt(i)){
-                    flag=1;
+            if(word.length()<=s.length()){
+                int flag=0;
+                for(int i=0;i<word.length();i++){
+                    if(s.charAt(i)!=word.charAt(i)){
+                        flag=1;
+                    }
                 }
-            }
-            if(flag!=1){
-                count++;
-                
+                if(flag!=1){
+                    count++;
+
+                }
             }
         }
         return count;
